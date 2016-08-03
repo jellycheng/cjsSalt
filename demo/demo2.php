b5ad1646543f7c32d065da63c9bec7d9948e635d
jellycheng <42282367@qq.com>
2016-08-03 18:33:04 +0800

    Create demo2.php

---
+++ demo/demo2.php
@@ -0,0 +1,22 @@
+<?php
+date_default_timezone_set("Asia/Shanghai");
+require  __DIR__.'/../vendor/autoload.php';
+
+$host = '10.59.80.11';
+$port = '8887';
+$user = 'saltapi';
+$pwd = 'saltapi';
+$obj = new \CjsSalt\CallSaltApi($host, $port);
+echo PHP_EOL;
+//$token = $obj->getToken($user, $pwd);
+//echo $token . PHP_EOL;
+$param = [
+    'fun'=>'test.ping',
+    //'tgt'=>'aaz-dev002',
+    //'args'=>'',
+    'user'=>$user,
+    'pwd'=>$pwd
+];
+$resData = $obj->callRunApi($param);
+var_export($resData);
+echo  PHP_EOL;
